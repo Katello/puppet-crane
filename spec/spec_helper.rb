956288e6945edc2f7b2bbe5e8b177f39f70c071d
Stephen Benjamin <stephen@redhat.com>
2016-10-27 12:50:08 -0400

    Modulesync, bump major for 1.8.7/el6 drop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,10 +6,12 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
-add_custom_fact :concat_basedir, '/tmp'
-add_custom_fact :mongodb_version, '2.4.14'
-add_custom_fact :root_home, '/root'
-add_custom_fact :puppetversion, Puppet.version
+                                                    # Original fact sources:
+add_custom_fact :concat_basedir, '/tmp'             # puppetlabs-concat
+add_custom_fact :mongodb_version, '2.4.14'          # puppetlabs-mongodb
+add_custom_fact :root_home, '/root'                 # puppetlabs-stdlib
+add_custom_fact :puppetversion, Puppet.version      # Facter, but excluded from rspec-puppet-facts
+add_custom_fact :puppet_environmentpath, Gem::Version.new(Puppet.version) >= Gem::Version.new('4.0') ? '/etc/puppetlabs/code/environments' : '' # puppetlabs-stdlib
 
 # Workaround for no method in rspec-puppet to pass undef through :params
 class Undef
@@ -31,6 +33,14 @@ end
 def exclude_test_os
   if ENV.key?('EXCLUDE_OS')
     ENV['EXCLUDE_OS'].split(',')
+  end
+end
+
+# Use the above environment variables to limit the platforms under test
+def on_os_under_test
+  on_supported_os.reject do |os, facts|
+    (only_test_os() && !only_test_os.include?(os)) ||
+      (exclude_test_os() && exclude_test_os.include?(os))
   end
 end
 
